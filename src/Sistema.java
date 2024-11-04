6f02aeac1aeef7113d9723eb3875837961879a4c
kristyson <131497756+kristyson@users.noreply.github.com>
2024-11-04 20:37:48 -0300

    Organizando as pastas

--- src/Sistema.java
+++
@@ -1,9 +0,0 @@
-public class Sistema{
-    public static void main(String[] args){
-        Medico Kristyson = new Medico("Kristyson", 21, "70556565312",  "Kris@gmai.com", "4002", "Otorrino");
-        Participante Leandro = new Participante(Kristyson);
-        System.out.println(Leandro.getIdade());
-        Participante participante = new Participante("Henrique", 15, "1234123", "daniel@hotmai.com", "espectador");
-        System.out.println(participante.getTipo());
-    }
-}
